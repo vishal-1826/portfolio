76bbf6d3f57dd11e5d9aebfbaae83e9d73a0b96f
vishal kumar verma <vv081997@gmail.com>
2024-07-17 12:40:37 +0530

    html worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,18 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolio';
+  myName: string = 'Vishal Kumar Verma';
+  jobSummary: string = "txt";
+  email: string = 'vishalkverma26@gmail.com';
+  phoneNumber: string = '9889907138';
+  linkedinLink: string =
+    'https://www.linkedin.com/in/vishal-kumar-verma-246289198/';
+  fbLink: string = 'https://www.facebook.com/yourprofile/';
+  angularVersion: string = 'X.X.X'; // Replace with your Angular version
+
+  constructor() {}
 }
